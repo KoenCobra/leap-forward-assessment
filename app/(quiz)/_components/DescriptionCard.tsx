7acd6ea464950fe32f11640f8e0f0a64738aaab3
Koen De Groot <koendegroot84@gmail.com>
2025-10-05 12:11:33 +0200

    Refactor DescriptionCard component: improve layout and styling by adding a border, enhancing icon size, and incorporating level information for better visual clarity.

--- app/(quiz)/_components/DescriptionCard.tsx
+++ app/(quiz)/_components/DescriptionCard.tsx
@@ -4,12 +4,21 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const DescriptionCard = () => {
   return (
-    <div className="rounded-2xl p-2 flex-1">
+    <div className="rounded-2xl flex-1 border border-blue-medium">
       <div className="bg-printing-houses bg-black/40 bg-blend-overlay bg-cover bg-center h-[25%] rounded-t-lg p-2">
-        <ContentElevated>
-          <FontAwesomeIcon icon={faStopwatch} className="text-2xl" />
-          42:32
-        </ContentElevated>
+        <div className="flex items-center gap-2">
+          <ContentElevated>
+            <div className="px-3 py-1.5 flex items-center gap-2">
+              <FontAwesomeIcon icon={faStopwatch} className="text-sm " />
+              <span className="font-md font-bold">42:32</span>
+            </div>
+          </ContentElevated>
+          <div className="flex-1">
+            <ContentElevated>
+              <span className="text-md font-bold">Level 2 / 8</span>
+            </ContentElevated>
+          </div>
+        </div>
       </div>
     </div>
   );
