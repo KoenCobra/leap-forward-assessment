595129ef16bd3bfa0f84574d1a5b8b70a06a86e9
Koen De Groot <koendegroot84@gmail.com>
2025-10-06 19:43:12 +0200

    Refactor button styling in ButtonElevated and ContentElevated components: enhance layout with grid properties for improved alignment and visual hierarchy, and adjust quiz page layout for better responsiveness.

--- components/ButtonElevated.tsx
+++ components/ButtonElevated.tsx
@@ -11,17 +11,24 @@ interface ButtonElevatedProps {
 const ButtonElevated = ({ icon, text, iconClassName }: ButtonElevatedProps) => {
   return (
     <>
-      <button className="p-3 rounded-md bg-primary-blue-darkest text-center cursor-pointer">
-        <FontAwesomeIcon
-          icon={icon && icon}
-          className={cn(iconClassName, "text-xl")}
-          style={{
-            strokeWidth: 30,
-            stroke: "currentColor",
-          }}
-        />
-        <span>{text}</span>
-      </button>
+      <div className="relative z-1">
+        <button
+          className="relative p-3 rounded-md bg-primary-blue-dark text-center cursor-pointer
+          grid place-items-center
+        after:content-[''] after:absolute after:inset-0 after:rounded-md  
+      after:translate-y-1 after:bg-primary-blue-darkest after:-z-1"
+        >
+          <FontAwesomeIcon
+            icon={icon && icon}
+            className={cn(iconClassName, "text-xl")}
+            style={{
+              strokeWidth: 30,
+              stroke: "currentColor",
+            }}
+          />
+          <span>{text}</span>
+        </button>
+      </div>
     </>
   );
 };
