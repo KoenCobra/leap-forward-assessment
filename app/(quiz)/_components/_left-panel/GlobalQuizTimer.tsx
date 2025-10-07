8f2c8d4fa89df0218c8482018a6198b6c5b017c3
Koen De Groot <koendegroot84@gmail.com>
2025-10-07 18:07:24 +0200

    Update GlobalQuizTimer component: replace countdown logic with static time display for simplified functionality in the quiz interface.

--- app/(quiz)/_components/_left-panel/GlobalQuizTimer.tsx
+++ app/(quiz)/_components/_left-panel/GlobalQuizTimer.tsx
@@ -3,18 +3,13 @@
 import ContentElevated from "@/components/ContentElevated";
 import { faStopwatch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useCountDown } from "@reactuses/core";
 
 const GlobalQuizTimer = () => {
-  const [_, minute, second] = useCountDown(50 * 60);
-
   return (
     <ContentElevated>
       <div className="px-3 py-1.5 flex items-center gap-2">
         <FontAwesomeIcon icon={faStopwatch} className="text-sm" />
-        <span className="font-md font-bold">
-          {minute}:{second}
-        </span>
+        <span className="font-md font-bold">42:32</span>
       </div>
     </ContentElevated>
   );
