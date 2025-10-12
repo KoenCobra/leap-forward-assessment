ddbd59c33c9500db4918cffd9415a7ccd6a7d0ad
Koen De Groot <koendegroot84@gmail.com>
2025-10-12 15:55:25 +0200

    Refactor RightPanel component: simplify the return statement by removing unnecessary div properties, enhancing code readability and maintaining layout consistency.

--- components/RightPanel.tsx
+++ components/RightPanel.tsx
@@ -1,11 +1,7 @@
 import React from "react";
 
 const RightPanel = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <div className="bg-blue-background rounded-2xl p-4 h-screen xl:h-full">
-      {children}
-    </div>
-  );
+  return <div className="bg-blue-background rounded-2xl p-4">{children}</div>;
 };
 
 export default RightPanel;
