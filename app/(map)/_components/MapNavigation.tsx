1b09a55692bc569b1e62fa2c184db22c348a21aa
Koen De Groot <koendegroot84@gmail.com>
2025-10-12 09:02:22 +0200

    Enhance MapNavigation and UnlockButton components: add LevelProgress display to MapNavigation for better user feedback and adjust UnlockButton positioning for improved layout consistency.

--- app/(map)/_components/MapNavigation.tsx
+++ app/(map)/_components/MapNavigation.tsx
@@ -1,8 +1,12 @@
+import LevelProgress from "@/components/LevelProgress";
 import UnlockButton from "./UnlockButton";
 
 const MapNavigation = () => {
   return (
-    <div className="relative h-full rounded-2xl bg-map bg-cover bg-center">
+    <div className="relative h-full rounded-2xl bg-map bg-cover bg-center p-3">
+      <div className="w-1/2 lg:w-1/3 xl:w-1/5">
+        <LevelProgress level={0} totalLevels={6} />
+      </div>
       <UnlockButton />
     </div>
   );
--- app/(map)/_components/UnlockButton.tsx
+++ app/(map)/_components/UnlockButton.tsx
@@ -6,7 +6,7 @@ const buttonPositions = [
   { top: 60, left: 70 },
   { top: 45, left: 15 },
   { top: 75, left: 40 },
-  { top: 20, left: 80 },
+  { top: 20, left: 70 },
 ];
 
 const UnlockButton = () => {
