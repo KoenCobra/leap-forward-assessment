206a5f6cd91476b1261d94e5ad131add06019111
Koen De Groot <koendegroot84@gmail.com>
2025-10-22 15:55:09 +0200

    Remove user authorization check from AdminLayout, simplifying the component structure.

--- app/(admin)/layout.tsx
+++ app/(admin)/layout.tsx
@@ -2,12 +2,6 @@ import { auth } from "@clerk/nextjs/server";
 import React from "react";
 
 const AdminLayout = async ({ children }: { children: React.ReactNode }) => {
-  const { userId } = await auth();
-
-  if (!userId) {
-    throw new Error("Unauthorized");
-  }
-
   return <div className="container mx-auto mt-4 px-8">{children}</div>;
 };
 
